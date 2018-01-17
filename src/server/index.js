32bf8c049e2ca2abf227f65d11b15b7a6e844f61
Cameron <bravermancj@gmail.com>
2018-01-16 19:29:11 -0500

    added basic nodejs server to serve as backend

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,19 @@
-var app = require('http').createServer();
-var io = module.exports.io = require('socket.io')(app)
+const express = require('express');
+const app = express();
 
+
+//declare port number
 const PORT = process.env.PORT || 3001
+
+//start server on PORT
+const server = app.listen(PORT, function(){
+    console.log('Connected to port ' + PORT + '...');
+});
+
+//Initialize Socket IO
+const io = module.exports.io = require('socket.io').listen(server);
+
+//Set up initial connection
+io.on('connection', function(socket) {
+    console.log('Connected to sockets ' + socket.id);
+});
