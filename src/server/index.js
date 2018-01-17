382f757da5df7ab29adf238d55c4b136f4f075ae
Cameron <bravermancj@gmail.com>
2018-01-17 15:11:34 -0500

    changed more server stuff

--- src/server/index.js
+++ src/server/index.js
@@ -7,12 +7,6 @@ const { ADD_USER, BAD_NAME } = require('../constants');
 //declare port number
 const PORT = process.env.PORT || 3001
 
-//start server on PORT
-server.listen(PORT, function(){
-    console.log('Connected to port ' + PORT + '...');
-});
-
-
 let connectedUsers = {};
 
 
@@ -21,9 +15,6 @@ app.use( express.static(__dirname + '/../../build'));
 //Set up initial connection and handle connections
 io.on('connection', function(socket) {
     console.log('Connected to socket ' + socket.id);
-
-
-
     socket.on(ADD_USER, (name) => {
         if (connectedUsers[name] === null){
             connectedUsers[name] = name;
@@ -31,4 +22,9 @@ io.on('connection', function(socket) {
             socket.emit(BAD_NAME);
         }
     });
+});
+
+//start server on PORT
+server.listen(PORT, function(){
+    console.log('Connected to port ' + PORT + '...');
 });
