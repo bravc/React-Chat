925d878e1332e29459e08f7bdc09ea605d7d27b2
Cameron <bravermancj@gmail.com>
2018-01-16 21:44:26 -0500

    changed background gradient

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,8 +24,8 @@ class Login extends Component {
         return (
             <div className="container">
                 <div id="form-login">
-                    <form onSubmit={this.handleSubmit} className="login-form col-md-4 col-md-offset-4 centered" id="form-login" >
-                        <div className="form-group">
+                    <form onSubmit={this.handleSubmit} className="login-form col-md-4 col-md-offset-4" id="form-login" >
+                        <div className="form-group rounded">
                                 <label htmlFor="nickname">Nickname</label>
                                 <input type="text" name="nickname" className="form-control" placeholder="Enter a username" />
                                 <div className="error">{error ? error:null}</div>
